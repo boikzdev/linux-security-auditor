f9068203f73231e83de6a9570b7080f73137768d
Boikuntha <boikuntha.dev@gmail.com>
2026-06-13 14:19:24 +0530

    Create port_scanner.sh to list open ports
    Add a port scanner script using nmap or ss

--- scripts/port_scanner.sh
+++ scripts/port_scanner.sh
@@ -1,1 +1,34 @@
+#!/bin/bash
 
+
+echo "{"
+
+echo '"open_ports":'
+
+
+if command -v nmap >/dev/null; then
+
+
+nmap -sV localhost \
+-oG - \
+| grep open \
+| awk '{print $2,$3,$4}' \
+| jq -R -s '
+split("\n")
+'
+
+
+else
+
+
+ss -tuln \
+| awk '{print $5}' \
+| jq -R -s '
+split("\n")
+'
+
+
+fi
+
+
+echo "}"
